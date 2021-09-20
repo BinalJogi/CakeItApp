7639c5984d0e6feda0b33910e7800f36545fde40
BinalJogi <padiabinal@gmail.com>
2021-09-20 14:55:09 +0530

    modified UI and integrated with ViewMode

--- CakeItApp/View/CakeListViewController.swift
+++ CakeItApp/View/CakeListViewController.swift
@@ -16,71 +16,45 @@ class CakeListViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
-    var i = 0
-    var cakes: [Cake] = []
-
+    var viewModel:CakeListViewModelType!  // ViewModel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         title = "🎂CakeItApp🍰"
         
-        let url = URL(string: "https://gist.githubusercontent.com/hart88/79a65d27f52cbb74db7df1d200c4212b/raw/ebf57198c7490e42581508f4f40da88b16d784ba/cakeList")!
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let decodedResponse = try? JSONDecoder().decode([Cake].self, from: data!) {
-                self.cakes = decodedResponse
-                self.tableView.reloadData()
-                return
-            }
-        }.resume()
+        viewModel.fetchCakes()
     }
-}
 
-extension CakeListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        i = indexPath.row
-        performSegue(withIdentifier: "segue", sender: tableView)
-    }
-    
 }
-
 extension CakeListViewController: UITableViewDataSource {
-    
-    
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return cakes.count
+        return viewModel.itemCount
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CakeTableViewCell
-        let cake = cakes[indexPath.row]
-        cell.titleLabel.text = cake.title
-        cell.descLabel.text = cake.desc
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as?  CakeTableViewCell else {
+            return UITableViewCell()
+        }
         
-        
-        
-        
-        let imageURL = URL(string: cake.image)!
-        
-        guard let imageData = try? Data(contentsOf: imageURL) else { return cell }
-            
-            let image = UIImage(data: imageData)
-            DispatchQueue.main.async {
-            cell.cakeImageView.image = image
-            }
+        let cake = viewModel.getCakes(for: indexPath.row)
+        cell.configure(with: cake)
         return cell
     }
 }
-
+extension CakeListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cake = viewModel.getCakes(for: indexPath.row)
+    }
+    
+}
 extension CakeListViewController: CakeListViewType {
     func updateUI() {
-        
+        tableView.reloadData()
     }
     
     func showError() {
-        
+        tableView.reloadData()
     }
 }
